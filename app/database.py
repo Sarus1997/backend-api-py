652f50cdac0fd933655c3e1c6b5ec9969136566b
S Suwannapaond <saharat.rus@gmail.com>
2024-12-07 09:54:58 +0700

    update

--- app/database.py
+++ app/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ตั้งค่าการเชื่อมต่อ
+#* ตั้งค่าการเชื่อมต่อ #
 DATABASE_URL = "mysql+mysqlconnector://root:root@localhost/employee_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# ฟังก์ชันสำหรับดึง session
+#* ฟังก์ชันสำหรับดึง session #
 def get_db():
     db = SessionLocal()
     try:
